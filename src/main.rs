e4357ca789b353ea5ba09d8c206206d3495835a5
David M. <david.mosquera.1@ulaval.ca>
2026-06-15 10:53:26 -0400

    feat: add Connection to workflow

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@
 use burn::backend::FlexDevice;
 use burn::tensor::Device;
 use burn_central_example::training::{self, MnistTrainingConfig};
-use tracel::{Context, experiment::ExperimentRun};
+use tracel::{Connection, Context, experiment::ExperimentRun};
 
 fn main() -> anyhow::Result<()> {
-    Context::cloud()?
+    Context::new(Connection::Cloud)?
         .experiment()
         .create("MNIST_Training", |session: &ExperimentRun, config| {
             training::run(session, config, vec![Device::autodiff(FlexDevice.into())])
-        })?
+        })
         .run(MnistTrainingConfig::default())
         .map_err(anyhow::Error::from_boxed)?;
 
